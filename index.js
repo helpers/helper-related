2031f95f58c0ddee1c158604b3e07dc48e9e3010
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-27 11:52:58 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+/*!
+ * helper-related <https://github.com/jonschlinkert/helper-related>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+var chalk = require('chalk');
+var async = require('async');
+var mdu = require('markdown-utils');
+var get = require('get-pkgs');
+
+module.exports = function (options) {
+  options = options || {};
+  var fn = options.linkify || linkify;
+
+  return function related(repos, patterns, cb) {
+    if (typeof patterns === 'function') {
+      cb = patterns;
+      patterns = '*';
+    }
+
+    if (typeof repos !== 'string' && !Array.isArray(repos)) {
+      throw new TypeError('helper-related expects a string or array.');
+    }
+
+    if (options && options.verbose) {
+      console.log(chalk.gray('%s'), '  helper-related: getting related projects from npm.');
+    }
+
+    get(repos, patterns, function (err, pkgs) {
+      if (err) {
+        // print out helper errors, they're harder to debug
+        console.error(chalk.red('helper-related: %j'), err);
+        return cb(err);
+      }
+      async.mapSeries(pkgs, function (pkg, next) {
+        next(null, fn(pkg));
+      }, function (err, arr) {
+        if (err) {
+        // print out helper errors, they're harder to debug
+          console.error(chalk.red('helper-related: %j'), err);
+          return cb(err);
+        }
+        cb(null, arr.join('\n'));
+      });
+
+    });
+  };
+};
+
+function linkify(pkg) {
+  return mdu.link(pkg.name, pkg.homepage, pkg.description)
+    + ': '+ pkg.description;
+}
---
+++ test.js
@@ -0,0 +1,49 @@
+/*!
+ * helper-related <https://github.com/jonschlinkert/helper-related>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+var assert = require('assert');
+var should = require('should');
+var related = require('./')();
+
+describe('related', function () {
+  it('should get a package.json from npm:', function (done) {
+    related('verb', function (err, res) {
+      res.should.match(/\[verb\]/);
+      done();
+    });
+  });
+
+  it('should get an array of package.json files:', function (done) {
+    related(['verb', 'assemble'], function (err, res) {
+      res.should.match(/\[verb\]/);
+      res.should.match(/\[assemble\]/);
+      done();
+    });
+  });
+
+  it('should filter package properties using glob patterns:', function (done) {
+    related(['verb', 'assemble'], ['hom*'], function (err, res) {
+      res.should.match(/https:\/\//);
+      done();
+    });
+  });
+
+  it('should filter package properties using glob patterns:', function (done) {
+    related(['verb', 'assemble'], ['name'], function (err, res) {
+      res.should.not.match(/https:\/\//);
+      done();
+    });
+  });
+
+  it('should throw an error:', function () {
+    (function () {
+      related();
+    }).should.throw('helper-related expects a string or array.');
+  });
+});
