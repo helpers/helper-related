2294139f50a5c672da29be2eccfc57a4e7c9eadf
jonschlinkert <jon.schlinkert@sellside.com>
2015-05-01 04:44:59 -0400

    Merge branch 'master' of https://github.com/tunnckoCore/helper-related
    Conflicts:
    	index.js
    	test.js

--- test.js
+++ test.js
@@ -50,11 +50,41 @@ describe('related', function () {
     });
   });
 
+  it('should truncate description to 15 words by default', function (cb) {
+    related(['micromatch', 'assemble'], function (err, res) {
+      res.should.equal([
+        '* [assemble](http://assemble.io): Static site generator for Grunt.js, Yeoman and Node.js. Used by Zurb Foundation, Zurb Ink, H5BP/Effeckt,… [more](http://assemble.io)',
+        '* [micromatch](https://github.com/jonschlinkert/micromatch): Glob matching for javascript/node.js. A drop-in replacement and faster alternative to minimatch and multimatch. Just… [more](https://github.com/jonschlinkert/micromatch)'
+      ].join('\n'));
+      cb();
+    });
+  });
+
   it('should truncate the description to the given number of words:', function (cb) {
     related(['remarkable', 'micromatch'], {words: 10}, function (err, res) {
       res.should.equal([
         '* [micromatch](https://github.com/jonschlinkert/micromatch): Glob matching for javascript/node.js. A drop-in replacement and faster alternative… [more](https://github.com/jonschlinkert/micromatch)',
         '* [remarkable](https://github.com/jonschlinkert/remarkable): Markdown parser, done right. 100% Commonmark support, extensions, syntax plugins,… [more](https://github.com/jonschlinkert/remarkable)'
+      ].join('\n'));
+      cb();
+    });
+  });
+
+  it('should truncate description to 15 when truncate:true', function (cb) {
+    related(['micromatch', 'assemble'], function (err, res) {
+      res.should.equal([
+        '* [assemble](http://assemble.io): Static site generator for Grunt.js, Yeoman and Node.js. Used by Zurb Foundation, Zurb Ink, H5BP/Effeckt,… [more](http://assemble.io)',
+        '* [micromatch](https://github.com/jonschlinkert/micromatch): Glob matching for javascript/node.js. A drop-in replacement and faster alternative to minimatch and multimatch. Just… [more](https://github.com/jonschlinkert/micromatch)'
+      ].join('\n'));
+      cb();
+    });
+  });
+
+  it('should not truncate description when truncate:false', function (cb) {
+    related(['micromatch', 'assemble'], {truncate: false}, function (err, res) {
+      res.should.equal([
+        '* [assemble](http://assemble.io): Static site generator for Grunt.js, Yeoman and Node.js. Used by Zurb Foundation, Zurb Ink, H5BP/Effeckt, Less.js / lesscss.org, Topcoat, Web Experience Toolkit, and hundreds of other projects to build sites, themes, components, documentation, blogs and gh',
+        '* [micromatch](https://github.com/jonschlinkert/micromatch): Glob matching for javascript/node.js. A drop-in replacement and faster alternative to minimatch and multimatch. Just use `micromatch.isMatch()` instead of `minimatch()`, or use `micromatch()` instead of `multimatch()`.'
       ].join('\n'));
       cb();
     });
